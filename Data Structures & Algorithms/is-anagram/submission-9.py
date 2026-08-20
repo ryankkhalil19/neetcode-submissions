2ec40c85a8c7ce411163832fbbd0a306ef9759c6
Ryan Khalil <ryankkhalil19@gmail.com>
2026-08-20 09:49:05 -0700

    Add: is-anagram - submission-9

---
+++ Data Structures & Algorithms/is-anagram/submission-9.py
@@ -0,0 +1,22 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        instanceMapS = {}
+        instanceMapT = {}
+
+        for char in s:
+            if char not in instanceMapS:
+                instanceMapS[char] = 1
+            else:
+                instanceMapS[char] += 1
+        
+        for char in t:
+            if char not in instanceMapT:
+                instanceMapT[char] = 1
+            
+            else:
+                instanceMapT[char] += 1
+
+        if instanceMapS == instanceMapT:
+            return True
+
+        return False
